0d7bbb7fe39af4075088770c74347f3af5e024b6
worthygod <115902278+worthygod@users.noreply.github.com>
2023-04-16 20:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,9 +151,9 @@ else :
     st.text("")
     st.text("")
     st.text("You can download some sample images by clicking on the below links :")
-    st.write("[link](https://drive.google.com/file/d/187K3FpyiTTcxDq8gFeE_U2E370BRbmNp/view?usp=share_link)")
-    st.write("[link](https://drive.google.com/file/d/1K1UD0YWfp1-dUmIrbOayAQIhKX7HKvqw/view?usp=share_link)")
-    st.write("[link](https://drive.google.com/file/d/14EafkfByi9XZ4tLJZ4O3JfQv1bWJmnYE/view?usp=share_link)")
+    st.write("[link](https://drive.google.com/file/d/187K3FpyiTTcxDq8gFeE_U2E370BRbmNp/view?usp=share)")
+    st.write("[link](https://drive.google.com/file/d/1K1UD0YWfp1-dUmIrbOayAQIhKX7HKvqw/view?usp=share)")
+    st.write("[link](https://drive.google.com/file/d/14EafkfByi9XZ4tLJZ4O3JfQv1bWJmnYE/view?usp=share)")
     st.text("")
     st.text("")
       
